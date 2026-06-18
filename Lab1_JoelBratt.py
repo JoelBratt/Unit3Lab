c87fdec5c3fc7875aff81187a44937c31da6e273
Joel Bratt <jbratt1@student.cscc.edu>
2026-06-17 21:34:43 -0400

    added the start of the loop menu

--- Lab1_JoelBratt.py
+++ Lab1_JoelBratt.py
@@ -1,5 +1,24 @@
 """
 Geomtery Calc
 Joel Bratt
-Calculates Circle area and circumfrence as well as retangle area and perimiter
+Calculates Circle Area and Circumference as well as retangle Area and Perimiter
 """
+selection = 0
+while selection != 5:
+    print("Geometry Calculator \n" \
+    "------------------------------------ \n" \
+    "1. Calculate Circle Area \n" \
+    "2. Calculate Circle Circumference \n" \
+    "3. Calculate Retangle Area \n" \
+    "4. Calculate Retangle Perimiter \n" \
+    "5. Exit \n" \
+    "Enter your selection 1-5" )
+
+    selection = input(int)
+
+    print(f"{selection} Great Choice!")
+
+
+
+
+
